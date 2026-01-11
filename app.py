140f37bd5066dbcb8da4bea6ecb4956889f76625
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-01-11 15:38:56 +0100

    Dodanie wymaganych scieżek

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from flask import Flask, render_template, request, redirect, url_for
 from katalog import Katalog
 from produkt import ProduktSztuki, ProduktWaga
@@ -11,26 +13,70 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 moj_katalog = Katalog()
 
 # Dodajemy dane startowe, żeby nie było pusto na start
-jajka = ProduktSztuki("Jajka", "2026-02-24", 10)
-mleko = ProduktWaga("Mleko", "2026-01-15", 1.5, "l")
+jajka = ProduktSztuki("Jajka", "2026-02-1", 10)
+mleko = ProduktWaga("Mleko", "2026-01-15", 1.5, "l", True)
 moj_katalog.addProdukt(jajka)
 moj_katalog.addProdukt(mleko)
 
 
+# ======= SCIEŻKI DO STRON =========
 @app.route('/')
 def strona_glowna():
-    # Pobieramy listę obiektów z Twojej klasy Katalog
-    lista = moj_katalog.getAll()
 
-    # Przekazujemy je do HTML
+    context = {
+        'product_count': moj_katalog.count_all(),
+        'close_date_count': moj_katalog.count_expiring_soon(),
+        # 'expired_count': moj_katalog.count_expired(),
+        'frozen_count': moj_katalog.count_frozen()
+    }
+
+    return render_template('dashboard.html', **context)
+
+@app.route('/list')
+def lista_produktow():
+    # Pobieramy listę obiektów z Katalog
+    lista = moj_katalog.getAll()
+    # Przekazanie do HTML'a
     return render_template('index.html', produkty=lista)
 
+
+@app.route('/zuzyj/<id_produktu>', methods=['GET', 'POST'])
+def zuzyj_produkt_strona(id_produktu):
+    # 1. Szukamy produktu
+    produkt = moj_katalog.getProduktById(id_produktu)
+
+    # Zabezpieczenie: jak nie ma takiego ID, wracamy na listę
+    if not produkt:
+        return redirect(url_for('lista_produktow'))
+
+    # === POST: Ktoś kliknął "Zapisz" ===
+    if request.method == 'POST':
+        try:
+            ile = float(request.form.get('zuzyta_ilosc'))
+
+            # Logika: odejmowanie
+            nowa_ilosc = produkt.ilosc - ile
+
+            # Nie pozwalamy na ujemne ilości
+            if nowa_ilosc < 0:
+                nowa_ilosc = 0
+
+            produkt.ilosc = nowa_ilosc
+
+        except ValueError:
+            pass  # Ignorujemy błędy
+
+        # Wracamy na listę zobaczyć efekt
+        return redirect(url_for('lista_produktow'))
+
+    # === GET: Ktoś wszedł na stronę ===
+    return render_template('zuzyj.html', p=produkt)
 
 @app.route('/usun/<id_produktu>')
 def usun_produkt(id_produktu):
     # Używamy Twojej metody z Katalogu
     moj_katalog.removeProduktById(id_produktu)
-    return redirect(url_for('strona_glowna'))
+    return redirect(request.referrer or url_for('strona_glowna'))
 
 
 @app.route('/dodaj', methods=['POST'])
@@ -51,13 +97,12 @@ def dodaj_produkt():
         nowy_produkt = ProduktSztuki(nazwa, data, int(ilosc))
     else:
         # Dla wagi/objętości przekazujemy wybraną jednostkę (kg, g, l, ml)
-        # Twoja klasa ProduktWaga sama sprawdzi czy jednostka jest poprawna!
         nowy_produkt = ProduktWaga(nazwa, data, ilosc, jednostka)
 
     # 3. Dodanie do katalogu
     moj_katalog.addProdukt(nowy_produkt)
 
-    return redirect(url_for('strona_glowna'))
+    return redirect(request.referrer or url_for('strona_glowna'))
 
 if __name__ == '__main__':
     app.run(debug=True)
